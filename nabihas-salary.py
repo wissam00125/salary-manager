360675276157a1f3d8f0e3241154ac1bf9a937fc
wissam00125 <wissamh1091@gmail.com>
2025-02-21 19:13:50 +0200

    Add extra savings input and update remaining balance and total expenses

--- nabihas-salary.py
+++ nabihas-salary.py
@@ -42,6 +42,28 @@ while start == 'yes':
     remainder = salary - total_expenses
     yearly_rent_cost = categories_amount[1] * 12
     yearly_electricity_cost = categories_amount[2] * 12
+    
+    while remainder > 0:
+        extra_saving = input("Do you have extra savings? (Yes/No) ")
+        if extra_saving == 'yes':
+            try:
+                extra_saving_amount = float(input('Enter the extra saving amount in USD: '))
+                if extra_saving_amount > 0 and extra_saving_amount <= remainder:
+                    remainder -= extra_saving_amount
+                    total_expenses += extra_saving_amount
+                    categories_amount[0] += extra_saving_amount
+                    break
+                else:
+                    print(f'You can save an additional amount up to ${remainder}')
+            except:
+                print('Please enter a numerical amount')
+        elif extra_saving == 'no':
+            break
+        else:
+            print('Please enter Yes or No only')
+            
+    categories_amount.clear()
+    categories_percentage.clear()      
     print('Total expenses: $', total_expenses)
     print('Remmainder: $', remainder)
     print('Yearly rent cost $', yearly_rent_cost)

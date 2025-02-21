f712e5cd2e4607b80cc19bd7a60c898eff180a18
wissam00125 <wissamh1091@gmail.com>
2025-02-21 15:58:23 +0200

    Validate category percentage input and handle errors

--- nabihas-salary.py
+++ nabihas-salary.py
@@ -15,6 +15,20 @@ while start == 'yes':
             else: print('Salary must be greater than zero. Please try again')
         except: print('Please enter a valid salary') 
     month = input('Please enter the name of the month: ')
+    
+    for i in range(3):
+        while True:
+            try:
+                percentage = float(input(f'Enter the percentage of {categories[i]}: '))
+                if percentage >= 0 and percentage <= 100:
+                    categories_percentage.append(percentage)
+                    amount = (percentage * salary) / 100
+                    categories_amount.append(amount)
+                    break # exit from loop
+                else:
+                    print("Percentage should be between 0 and 100")
+            except:
+                print("Please enter a numerical value")
     start = input('Do you want to continue? (Yes/No) ')
 
 

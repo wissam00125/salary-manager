ab0dae1160dd932a8185b30eb8bf33bb5ab11d0d
wissam00125 <wissamh1091@gmail.com>
2025-02-21 12:31:47 +0200

    Request salary and month inputs

--- nabihas-salary.py
+++ nabihas-salary.py
@@ -1,5 +1,18 @@
 # Salary Manager project
+categories = ['Savings', 'Rent', 'Electricity']
+categories_amount = []
+categories_percentage = []
 
+start = input('Do you want to start? (Yes/No) ')
+
+while start == 'yes':
+    salary = input('please enter the salary: ')
+    salary = float(salary)
+    if type(salary)  != float or salary <= 0:
+        print('Invalid salary.')
+        break  # exit from loop
+    month = input('Please enter the name of the month: ')
+    start = input('Do you want to continue? (Yes/No) ')
 
 
 
